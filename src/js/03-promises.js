0414f3b5395689c8104ea69bb09216e7937712b8
Olga Bieliaieva <belyaevaolya0@gmail.com>
2023-02-26 20:01:45 +0200

    some changes

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,14 +1,21 @@
 const refs = {
-  form: document.querySelector(".form")
-}
+  form: document.querySelector('.form'),
+  formBtn: document.querySelector('form button'),
+};
 console.log(refs.form);
+console.log(refs.formBtn);
 
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+refs.formBtn.addEventListener('submit', createPromise);
+
+function createPromise(event, position, delay) {
+  event.preventDefault();
+  console.log(event);
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      console.log("it's OK");
+    } else {
+      console.log("it's bad");
+    }
+  });
 }
-// event.preventDefault();
