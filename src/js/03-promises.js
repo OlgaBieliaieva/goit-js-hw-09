ba04f8f500b0d3d0c154c6da31a2eb9a44bae6ab
Olga Bieliaieva <belyaevaolya0@gmail.com>
2023-03-03 18:19:15 +0200

    finished task #3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,104 +1,43 @@
-let timerId = null;
-let intervalId = null;
-let position = null;
-
-
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
-  form: document.querySelector('.form'),  
+  form: document.querySelector('.form'),
 };
 
 refs.form.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(event) {
-  event.preventDefault();  
+  event.preventDefault();
 
-  const userDelay = Number(event.target.elements['delay'].value);
-  const userInterval = Number(event.target.elements['step'].value);
+  const userStep = Number(event.target.elements['step'].value);
   const userAmount = Number(event.target.elements['amount'].value);
+  let delay = Number(event.target.elements['delay'].value);
 
-  timerId = setTimeout(() => {
-    position += 1;
-    createPromise(position, userDelay)
-    console.log(position);
-    console.log(userDelay);
-    intervalId = setInterval(() => {      
-      position += 1;
-      createPromise(position, userInterval)
-      console.log(position);
-      console.log(userInterval);
-      if (position > userAmount - 1) {
-        clearInterval(intervalId);
-      }
-    }, userInterval), userDelay
-})    
+  formHandler(delay, userStep, userAmount);
+}
+
+function formHandler(delay, userStep, userAmount) {
+  for (let i = 1; i <= userAmount; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += userStep;
+  }
 }
 
 function createPromise(position, delay) {
-  
-  const promises = new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
-    if (shouldResolve) {
-      resolve ({position, delay})
-    }
-    else {
-      reject ({position, delay})
-    }
-  })
-  console.log(promises);
-  return promises;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
-
-Promise.all(promises)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({position, delay}) => {
-  console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-});
-
-// function onFormSubmit(event) {
-//   event.preventDefault();
-//   delay = Number(event.target.elements['delay'].value);
-//   interval = Number(event.target.elements['step'].value);
-//   const userAmount = Number(event.target.elements['amount'].value);
-//   timerId = setTimeout(() => {
-//     position += 1;
-//     console.log(position);
-//     console.log(delay);
-//     intervalId = setInterval(() => {      
-//       position += 1;
-//       console.log(position);
-//       console.log(interval);
-//       if (position > userAmount - 1) {
-//         clearInterval(intervalId);
-//       }
-//     }, interval), delay
-// })    
-// }
-
-
-
-// const promise = new Promise((resolve, reject) => {
-//   const canFulfill = Math.random() > 0.5;
-
-//   setTimeout(() => {
-//     if (canFulfill) {
-//       resolve('Промис выполнился успешно, с результатом (исполнен, fulfilled)');
-//     }
-
-//     reject('Промис выполнился с ошибкой (отклонён, rejected)');
-//   }, 1000);
-// });
-
-// promise.then(onFulfilled, onRejected);
-
-// function onFulfilled(result) {
-//   console.log('onFulfilled -> onFulfilled');
-//   console.log(`✅ ${result}`);
-// }
-
-// function onRejected(error) {
-//   console.log('onRejected -> onRejected');
-//   console.log(`❌ ${error}`);
-// }
